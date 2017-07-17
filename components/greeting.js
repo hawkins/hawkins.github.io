e1148c21a3301afeb2480e829dbf85be0fc3810a
Josh Hawkins <jkhaccounts@gmail.com>
2017-07-16 20:21:54 -0500

    Correct greeting

--- components/greeting.js
+++ components/greeting.js
@@ -4,8 +4,13 @@ export default () =>
   <div>
     <h2>Hi there!</h2>
     <p>
-      Thanks for stopping by - feel free to check out the blog posts below to
-      see what I'm up to lately. Or if you're interested to see some of my
+      Thanks for stopping by - feel free to check out my
+      {" "}
+      <Link href="/a-writer">
+        <a>blog</a>
+      </Link>
+      {" "}
+      to see what I'm up to lately. Or if you're interested to see some of my
       projects, you can check out my
       {" "}
       <Link href="/an-open-source-fanatic">

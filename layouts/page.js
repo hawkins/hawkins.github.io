7f8c817b38e2635e1a37d79a7d0d3e80b43aafcd
Josh Hawkins <jkhaccounts@gmail.com>
2018-01-26 22:07:04 -0500

    Use NOW_URL

--- layouts/page.js
+++ layouts/page.js
@@ -5,6 +5,8 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import PropTypes from "prop-types";
 
+const { NOW_URL } = process.env;
+
 const Page = ({ title, children }) => (
   <div className="page">
     <Head>
@@ -18,12 +20,7 @@ const Page = ({ title, children }) => (
       <meta property="og:locale" content="en_US" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Josh Hawkins is..." />
-      <meta property="og:image" content="http://hawkins.is/static/og.jpg" />
-      <meta property="og:image:url" content="http://hawkins.is/static/og.jpg" />
-      <meta
-        property="og:image:secure_url"
-        content="https://hawkins.is/static/og.jpg"
-      />
+      <meta property="og:image" content={NOW_URL + "/static/og.jpg"} />
       <meta property="og:image:type" content="image/jpeg" />
       <meta property="og:image:height" content="200" />
       <meta property="og:image:width" content="200" />
@@ -32,7 +29,7 @@ const Page = ({ title, children }) => (
         content="Josh Hawkins performing a skateboard trick"
       />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:image" content="http://hawkins.is/static/og.jpg" />
+      <meta name="twitter:image" content={NOW_URL + "/static/og.jpg"} />
       <meta name="twitter:site" content="@hawkinjs" />
       <meta name="twitter:site:id" content="@hawkinjs" />
       <meta name="twitter:creator" content="@hawkinjs" />

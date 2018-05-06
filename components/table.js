f9a36484bfd5c67953ed963eafc6a5a1d5387266
Josh Hawkins <hawkins@users.noreply.github.com>
2018-05-05 20:05:28 -0400

    Add responsive table

---
+++ components/table.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+
+// Big thanks to https://codepen.io/nhackley/pen/QbGoLN for this nifty Table :)
+
+class Table extends Component {
+  render() {
+    const { rows, columns } = this.props;
+
+    return [
+      <table className="responsive-table" key="table">
+        <thead>
+          <tr>{Object.keys(columns).map(col => <th>{columns[col]}</th>)}</tr>
+        </thead>
+        <tbody>
+          {rows.map(row => (
+            <tr>
+              {Object.keys(columns).map(column => (
+                <td data-label={columns[column]}>{row[column]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>,
+      <style key="style">{`.responsive-table {
+        width: 100%;
+        margin: 0;
+        padding: 0;
+        border-collapse: collapse;
+        border-spacing: 0;
+      }
+      .responsive-table thead {
+        display: none;
+        background: #fafafa;
+      }
+      @media screen and (min-width: 700px) {
+        .responsive-table thead {
+          display: table-header-group;
+        }
+      }
+      .responsive-table tr {
+        border: 1px solid #ddd;
+        border-bottom: 2px solid #ddd;
+        padding: 5px;
+        margin-bottom: 10px;
+        display: block;
+      }
+      @media screen and (min-width: 700px) {
+        .responsive-table tr {
+          display: table-row;
+          border-bottom-width: 1px;
+          margin-bottom: 0;
+        }
+        .responsive-table tr:nth-child(even) {
+          background: #fafafa;
+        }
+      }
+      .responsive-table th,
+      .responsive-table td {
+        padding: 10px;
+        text-align: left;
+      }
+      .responsive-table td {
+        display: block;
+        text-align: right;
+        border-bottom: 1px dotted #ff0080;
+      }
+      .responsive-table td:last-child {
+        border-bottom: none;
+      }
+      @media screen and (min-width: 700px) {
+        .responsive-table td {
+          display: table-cell;
+          text-align: left;
+          border-bottom: none;
+        }
+      }
+      .responsive-table td:before {
+        content: attr(data-label);
+        float: left;
+        font-weight: bold;
+      }
+      @media screen and (min-width: 700px) {
+        .responsive-table td:before {
+          content: "";
+          display: none;
+        }
+      }
+      `}</style>
+    ];
+  }
+}
+
+// TODO: Proptypes
+
+module.exports = Table;
